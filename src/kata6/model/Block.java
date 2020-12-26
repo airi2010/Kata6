8e9c6d5031b2e980ea730eb722c8e5123636d8cd
Airam <Airam@DESKTOP-KN2Q17J>
2020-12-26 17:37:20 +0000

    Kata6 Update time Button When you press T button the block moves automaticaly

---
+++ src/kata6/model/Block.java
@@ -0,0 +1,86 @@
+package kata6.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Block {
+    public static final int MAX = 7;
+    private int x;
+    private int y;
+    private final Timer timer;
+    private final List<Observer> observers;
+    public Block() {
+        this.x = 4;
+        this.y = 4;
+        this.timer = new Timer();
+        this.observers = new ArrayList<>();
+    }
+    
+    public void left(){
+        if (this.x == 1) return;
+        this.x--;
+        changed();
+    }
+    
+    public void right(){
+        if (this.x == MAX) return;
+        this.x++;
+        changed();
+    }
+    
+    public void up(){
+        if (this.y == MAX-1) return;
+        this.y++;
+        changed();
+    }
+    
+    public void down(){
+        if (this.y == 0) return;
+        this.y--;
+        changed();
+    }
+    public void time(){
+        this.timer.schedule(task(), 1000, 500);
+    }
+    public void stop(){
+        this.timer.cancel();
+    }
+    public int x(){
+        return x;
+    }
+    public int y(){
+        return y;
+    }
+
+    private TimerTask task() {
+        return new TimerTask(){
+            @Override
+            public void run(){
+                double r = Math.random();
+                if (r >= 0.20) return;
+                else if (r >= 0.15) left();
+                else if (r >= 0.10) right();
+                else if (r >= 0.05) up();
+                else if (r >= 0.00) down();
+                for (Observer observer : observers) {
+                    observer.changed();
+                }
+            }
+        };
+    }
+
+    public void register(Observer observer) {
+        observers.add(observer);
+    }
+    
+    private void changed(){
+        for (Observer observer : observers) {
+            observer.changed();
+        }
+    }
+    public interface Observer {
+        void changed();
+    }
+}
---
+++ src/kata_6/Kata_6.java
@@ -0,0 +1,87 @@
+
+package kata_6;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import kata6.control.Command;
+import kata6.control.DownCommand;
+import kata6.control.LeftCommand;
+import kata6.control.RightCommand;
+import kata6.control.UpCommand;
+import kata6.control.timeCommand;
+import kata6.model.Block;
+import kata6.view.BlockDisplay;
+
+public class Kata_6 extends JFrame {
+    private final Block block;
+    private BlockDisplay blockDisplay;
+    Map<String, Command> commands;
+    public static void main(String[] args) {
+        new Kata_6().execute();
+    }
+    public Kata_6(){
+        this.block = new Block();
+        this.setTitle("Block shifter v1");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(740,790);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.getContentPane().add(blockPanel());
+        this.add(toolbar(),BorderLayout.SOUTH);
+        this.commands = createCommands();
+    }
+
+    private void execute() {
+        this.setVisible(true);
+    }
+
+    private JPanel blockPanel() {
+        BlockPanel panel = new BlockPanel();
+        this.blockDisplay = panel;
+        panel.display(block);
+        this.block.register(panel);
+        return panel;
+    }
+    
+    private Map<String,Command> createCommands(){
+        Map<String, Command> commands = new HashMap<>();
+        commands.put("U", new UpCommand(block));
+        commands.put("D", new DownCommand(block));
+        commands.put("L", new LeftCommand(block));
+        commands.put("R", new RightCommand(block));
+        commands.put("T", new timeCommand(block));
+        return commands;
+    }
+
+    private JMenuBar toolbar() {
+        JMenuBar result = new JMenuBar();
+        result.setLayout(new FlowLayout(FlowLayout.CENTER));
+        result.add(button("L"));
+        result.add(button("U"));
+        result.add(button("D"));
+        result.add(button("R"));
+        result.add(button("T"));
+        return result;
+    }
+
+    private JButton button(String command) {
+        JButton button = new JButton(command);
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent arg0){
+                commands.get(command).execute();
+            }
+        });
+        return button;
+    }
+    
+}
